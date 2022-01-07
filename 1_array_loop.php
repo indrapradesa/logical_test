367794ca053ee8077a8cbb75ec0a21ff96c35f53
indrapradesa <95635253+indrapradesa@users.noreply.github.com>
2022-01-07 11:13:29 +0700

    Add files via upload

---
+++ 1_array_loop.php
@@ -0,0 +1,10 @@
+<?php
+
+$aplikasi = ["gtAkademik", "gtFinansi", "gtPerizinan", "eCampuz", "eOviz"];
+$arrayLength = count($aplikasi);
+
+$i = 0;
+while ($i < $arrayLength) {
+	echo "$aplikasi[$i] ";
+	$i++;
+}
---
+++ 4_looping_condition.php
@@ -0,0 +1,18 @@
+<?php
+$n1 = 6;
+$n2 = 23;
+
+function bitwiseAND($n1, $n2)
+{
+    return $n1 & $n2;
+}
+
+function bitwiseOR($n1, $n2)
+{
+    return $n1 | $n2;
+}
+
+function bitwiseXOR($n1, $n2)
+{
+    return $n1 ^ $n2;
+}
